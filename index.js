b9778cae39ac19ddb52dccd530a8837edc905bb9
Hkishan01 <hky3122003@gmial.com>
2023-07-26 23:23:10 +0530

    Last commit

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express();
 const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,8 +31,8 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 const notesSchema = new mongoose.Schema({
-  title: String,
-  content: String,
+  User: String,
+  Notes: [{ Title: String, Content: String }]
 });
 const Notes = mongoose.model("Notes", notesSchema);
 
@@ -52,15 +53,6 @@ app.get("/login", (req, res) => {
 app.get("/notes", (req, res) => {
   res.sendFile("./notes.html", { root: __dirname });
 });
-app.get("/getData", async (req, res) => {
-  try {
-    const data = await Notes.find();
-    res.json(data);
-  } catch (error) {
-    console.log("error");
-  }
-});
-
 app.get("/getUser", async (req, res) => {
   try {
     const data = await User.find();
@@ -70,46 +62,106 @@ app.get("/getUser", async (req, res) => {
   }
 });
 
+
+
+app.post("/matchPassword", async (req, res) => {
+  try {
+    const { hashed, original } = req.body;
+    const bool = await bcrypt.compare(original, hashed);
+    res.json(bool)
+  } catch (e) {
+    res.json(false)
+    console.log(e)
+  }
+})
+
+app.post("/deleteNotes", async (req, res) => {
+  try {
+    const { Id, email } = req.body
+    await Notes.updateOne({ User: email }, { $pull: { Notes: { _id: Id } } })
+    res.json({ done: true })
+  } catch (error) {
+    res.json({ done: false })
+    console.log(error)
+  }
+})
+
+app.post("/editNotes", async (req, res) => {
+  try {
+    const { Id, email, Content } = req.body
+    await Notes.updateOne({ "Notes._id": Id }, { $set: { "Notes.$.Content": Content } })
+    res.json({ done: true })
+  } catch (error) {
+    res.json({ done: false })
+    console.log(error)
+  }
+})
+
+app.post("/getData", async (req, res) => {
+  try {
+    const user = req.body.Email
+    const data = await Notes.findOne({ User: user });
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+app.post("/userAuth", (req, res) => {
+  try {
+    let token = req.body.token;
+    const decode = jwt.verify(token, "digitalnotes")
+    res.json({ Login: true, data: decode })
+  } catch {
+    res.json({ Login: false, data: null })
+  }
+})
+
 // post api
 
-app.post("/register", (req, res) => {
-  const { name, email, password, cpassword } = req.body;
-  User.findOne({ Email: email })
-    .then((docs) => {
-      User.create({
-        userName: name,
-        Email: email,
-        password: password,
-      });
-      res.sendFile("./notes.html", { root: __dirname });
-      console.log("done new");
-    })
-    .catch((e) => {
-      console.log(e);
+app.post("/login", async (req, res) => {
+
+  try {
+    const { name, email, password, cpassword } = req.body;
+    const hashed = await bcrypt.hash(password, 10)
+    const bool = await bcrypt.compare("12", hashed)
+    await User.create({
+      userName: name,
+      Email: email,
+      password: hashed,
     });
-});
+    res.sendFile("./login.html", { root: __dirname });
+  } catch (e) {
+    console.log(e)
+  }
 
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  User.findOne({ Email: email })
-    .then((doc) => {
-      if (!doc) {
-        res.sendFile("/register.html", { root: __dirname });
-      }
-      const token = jwt.sign({ Email: email }, "digtalnotes");
-      res.sendFile("./notes.html", { root: __dirname });
+});
+let token;
+app.post("/getToken", async (req, res) => {
+  token = jwt.sign(req.body, "digitalnotes", { expiresIn: 86400 })
+  res.json(token)
+})
+
+app.post("/notes/v1", async (req, res) => {
+  const { title, content, email } = req.body;
+
+  let userPresent = await Notes.find({ User: email })
+
+  if (userPresent.length == 0) {
+    await Notes.create({
+      User: email,
+      Notes: [{
+        Title: title,
+        Content: content
+      }]
     })
-    .catch((e) => console.log(e));
-});
+  } else {
+    await Notes.updateOne({ User: email }, { $push: { Notes: [{ Title: title, Content: content }] } })
+  }
 
-app.post("/notes", (req, res) => {
-  const { title, content } = req.body;
-  Notes.create({
-    title: title,
-    content: content,
-  });
-  res.sendFile("./notes.html", { root: __dirname });
-  console.log("added successfully");
+  res.json({ done: true });
 });
 
 app.listen(3000, () => {
